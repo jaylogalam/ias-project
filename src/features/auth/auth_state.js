d67f7f6c0f1ecbc3c372f393f1bdf94b775f4e4a
Jaylo <jaylogalam04@gmail.com>
2025-09-03 11:39:20 +0800

    cleared files

--- src/features/auth/auth_state.js
+++
@@ -1,8 +0,0 @@
-import { create } from "zustand";
-
-export const useAuthStore = create((set) => ({
-  username: "",
-  password: "",
-  setUsername: (username) => set(() => ({ username: username })),
-  setPassword: (password) => set(() => ({ password: password })),
-}));
