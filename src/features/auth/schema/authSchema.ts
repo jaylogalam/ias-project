d226f411c1cd023ef78f484c3c1fda077fde2ec3
Jaylo <jaylogalam04@gmail.com>
2025-09-03 22:25:13 +0800

    change password min 5 to 8

--- src/features/auth/schema/authSchema.ts
+++ src/features/auth/schema/authSchema.ts
@@ -2,7 +2,7 @@ import { z } from "zod";
 
 export const signupSchema = z.object({
   username: z.string().nonempty("Username cannot be blank").min(5, "Username must contain atleast 5 characters").max(16),
-  password: z.string().nonempty("Password cannot be blank").min(5, "Password must contain atleast 5 characters").max(16),
+  password: z.string().nonempty("Password cannot be blank").min(8, "Password must contain atleast 8 characters").max(16),
 });
 
 export const loginSchema = z.object({
