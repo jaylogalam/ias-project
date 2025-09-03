97f294536a5e1ced5ad77ef482d61b1d9a0ccc8f
Jaylo <jaylogalam04@gmail.com>
2025-09-04 00:28:44 +0800

    stronger password schema

--- src/features/auth/schema/authSchema.ts
+++ src/features/auth/schema/authSchema.ts
@@ -1,8 +1,20 @@
 import { z } from "zod";
 
 export const signupSchema = z.object({
-  username: z.string().nonempty("Username cannot be blank").min(5, "Username must contain atleast 5 characters").max(16),
-  password: z.string().nonempty("Password cannot be blank").min(8, "Password must contain atleast 8 characters").max(16),
+  username: z
+    .string()
+    .nonempty("Username cannot be blank")
+    .min(5, "Too short")
+    .max(16),
+  password: z
+    .string()
+    .nonempty("Password cannot be blank")
+    .min(8, "Too short")
+    .max(16)
+    .regex(/[A-Z]/, "one uppercase letter")
+    .regex(/[a-z]/, "one lowercase letter")
+    .regex(/[0-9]/, "one number")
+    .regex(/[^A-Za-z0-9]/, "one special character"),
 });
 
 export const loginSchema = z.object({
