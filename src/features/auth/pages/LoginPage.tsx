2bc6d42022c88b5b6c68465accf5512cb5014768
Jaylo <jaylogalam04@gmail.com>
2025-09-03 13:18:51 +0800

    fixed sizes on smaller screens

--- src/features/auth/pages/LoginPage.tsx
+++ src/features/auth/pages/LoginPage.tsx
@@ -2,7 +2,7 @@ import { LoginForm } from "../components/LoginForm";
 
 function LoginPage() {
     return (
-      <div className="w-1/6">
+      <div className="w-1/6 min-w-96">
         <LoginForm />
       </div>
     );
--- src/features/auth/pages/SignupPage.tsx
+++ src/features/auth/pages/SignupPage.tsx
@@ -2,7 +2,7 @@ import { SignupForm } from "../components/SignupForm";
 
 function SignupPage() {
     return (
-      <div className="w-1/6">
+      <div className="w-1/6 min-w-96">
         <SignupForm />
       </div>
     );
