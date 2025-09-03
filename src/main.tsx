e1b13066d306860b7c9da3dbd6d244cdce6a631e
Jaylo <jaylogalam04@gmail.com>
2025-09-03 12:53:34 +0800

    added router provider

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import '@/app/config/theme/index.css'
 import App from './App.js'
+import { BrowserRouter } from "react-router-dom";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </StrictMode>
+);
