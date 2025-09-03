e0564dfefaa9e7ec2f17d953002e734181d6ceec
Jaylo <jaylogalam04@gmail.com>
2025-09-04 00:44:16 +0800

    fixed bug

--- src/features/auth/components/PasswordRequirements.tsx
+++ src/features/auth/components/PasswordRequirements.tsx
@@ -1,21 +1,27 @@
 function PasswordErrors(errors: any) {
     const errorList = errors.errors.invalid_format
-    
-    return (
+    const length = errors.errors.too_small
+
+    if (errorList) return (
       <div>
         <p className="text-sm pb-1 text-red-600">Must contain the atleast: </p>
-            {Array.isArray(errorList) ?
-                (errorList.map((e: string, i: number) => (
-                    <p key={i} className="px-3 py-1 mt-[-0.4rem] text-sm text-red-600">
-                        {`• ${e}`}
-                    </p>)
-                )
-                ) : (<p className="px-3 py-1 mt-[-0.4rem] text-sm text-red-600">
-                    {`• ${errorList}`}
-                </p>)
-            }
+        {showRequirements(errorList)}
       </div>
     );
+    else if (length) return (
+      <p className="px-3 py-1 mt-[-0.4rem] text-sm text-red-600">{length}</p>
+    );
+    
 }
 
 export default PasswordErrors
+
+function showRequirements(list: Array<string> | string) {
+    if (Array.isArray(list)) return list.map((e: string, i: number) => (
+      <p key={i} className="px-3 py-1 mt-[-0.4rem] text-sm text-red-600">
+        {`• ${e}`}
+      </p>
+    ));
+
+    return <p className="px-3 py-1 mt-[-0.4rem] text-sm text-red-600">{`• ${list}`}</p>;
+}
