b7c43f0013fd97e8e0edc5d323c43ff02c4f8baf
jaylo <jaylogalam04@gmail.com>
2025-09-05 14:47:08 +0800

    moved signup

--- src/features/auth/pages/SignupPage.tsx
+++ src/features/auth/pages/SignupPage.tsx
@@ -1,11 +1,11 @@
-import SignupForm from "../components/SignupForm";
+import SignupForm from "../components/signup/SignupForm";
 
 function SignupPage() {
-    return (
-      <div className="w-1/6 min-w-96">
-        <SignupForm />
-      </div>
-    );
+  return (
+    <div className="w-1/6 min-w-96">
+      <SignupForm />
+    </div>
+  );
 }
 
-export default SignupPage
+export default SignupPage;
