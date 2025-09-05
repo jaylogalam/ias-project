68b4114adab7492a942aa6a70d74eed2436c9f70
jaylo <jaylogalam04@gmail.com>
2025-09-05 14:48:19 +0800

    format changes

--- src/features/auth/server/fetchLoginRequest.ts
+++ src/features/auth/server/fetchLoginRequest.ts
@@ -6,19 +6,16 @@ export function fetchLoginRequest() {
   });
 }
 
-async function loginRequest(account: {
-  username: string;
-  password: string;
-}) {
+async function loginRequest(account: { username: string; password: string }) {
   const res = await fetch("http://localhost:5000/api/login", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify(account),
   });
 
-    const data = await res.json();
-    if (!res.ok) {
-        throw new Error(data.message)
-    };
-    return {data};
+  const data = await res.json();
+  if (!res.ok) {
+    throw new Error(data.message);
+  }
+  return { data };
 }
