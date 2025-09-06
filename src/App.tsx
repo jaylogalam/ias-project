9e7dd80449e07395736d9f20a0e4f2f2b67436af
Jaylo <jaylogalam04@gmail.com>
2025-09-06 18:50:15 +0800

    add recovery page

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 import LoginPage from "./features/auth/pages/LoginPage";
 import SignupPage from "./features/auth/pages/SignupPage";
+import RecoveryPage from "./features/auth/pages/RecoveryPage";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<LoginPage />} />
       <Route path="/signup" element={<SignupPage />} /> 
+      <Route path="/recovery" element={<RecoveryPage />} />
     </Routes>
   );
 }
---
+++ src/features/auth/pages/RecoveryPage.tsx
@@ -0,0 +1,11 @@
+import RecoverForm from "../components/recover/RecoveryForm";
+
+function RecoveryPage() {
+  return (
+    <div className="w-1/6 min-w-96">
+      <RecoverForm />
+    </div>
+  );
+}
+
+export default RecoveryPage;
