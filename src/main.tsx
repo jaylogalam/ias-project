3e261df73e981e6817d2dfeff43365eaa58d4bc4
Jaylo <jaylogalam04@gmail.com>
2025-09-06 20:17:05 +0800

    remove strict mode

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,3 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import '@/app/config/theme/index.css'
 import App from './App.js'
@@ -8,11 +7,9 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
-  <StrictMode>
     <QueryClientProvider client={queryClient}>
       <BrowserRouter>
         <App />
       </BrowserRouter>
     </QueryClientProvider>
-  </StrictMode>
 );
